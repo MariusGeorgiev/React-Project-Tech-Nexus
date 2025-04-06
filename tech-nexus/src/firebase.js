13b5e93eb6c8c799616352b3bc500ff7d6050978
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2025-04-06 06:40:27 +0300

    move firebase config secrets to .env file

--- tech-nexus/src/firebase.js
+++ tech-nexus/src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBw03jPwD2NccCz_ysHMR0bz4DU0WhhwYw",
-  authDomain: "technexus-react-project.firebaseapp.com",
-  projectId: "technexus-react-project",
-  storageBucket: "technexus-react-project.firebasestorage.app",
-  messagingSenderId: "395944107949",
-  appId: "1:395944107949:web:82dea050f52cf587e30b24"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
