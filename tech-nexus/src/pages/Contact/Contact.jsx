ace3de87774a506a018ce9a7aa1254903f331185
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2025-04-07 06:46:55 +0300

    contact page styles adjustments

--- tech-nexus/src/pages/Contact/Contact.jsx
+++ tech-nexus/src/pages/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { auth } from '../../firebase';
 import emailjs from 'emailjs-com';
-import './Contact.module.css';
+import styles from './Contact.module.css';
 
 const Contact = () => {
 
@@ -63,34 +63,34 @@ const Contact = () => {
 
   return (
     
-    <div className="contact-page">
-      <div className="first-section">
-        <div className="contact-container-left">
-          <p><strong>Email: </strong> <a href="mailto:alphacentauri6677@gmail.com"><i className="fas fa-envelope"></i> alphacentauri6677@gmail.com</a></p>
-          <p><strong>GitHub: </strong> <a href="https://github.com/MariusGeorgiev" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i> MariusGeorgiev</a></p>
+    <div className={styles["contact-page"]}>
+      <div className={styles["first-section"]}>
+        <div className={styles["contact-container-left"]}>
+          <p><strong>Email: </strong> <a href="mailto:alphacentauri6677@gmail.com"><i className={styles["fas fa-envelope"]}></i> alphacentauri6677@gmail.com</a></p>
+          <p><strong>GitHub: </strong> <a href="https://github.com/MariusGeorgiev" target="_blank" rel="noopener noreferrer"><i className={styles["fab fa-github"]}></i> MariusGeorgiev</a></p>
         </div>
 
-        <div className="contact-container-center">
+        <div className={styles["contact-container-center"]}>
           <h1>Contacts</h1>
-          <div className="profile-picture">
+          <div className={styles["profile-picture"]}>
             <img src="../../src/assets/tech-nexus.PNG" alt="pic" />
           </div>
           <p>
             <strong>Address:</strong>
             <a href="https://maps.app.goo.gl/6Y7mCCDwSXpazzdA6" target="_blank" rel="noopener noreferrer">
-              <i className="fas fa-map-marker-alt"></i> zh.k. Lyulin 1, ul. "Gen. Vasil Kutinchev, 1343", Sofia, Bulgaria
+              <i className={styles["fas fa-map-marker-alt"]}></i> zh.k. Lyulin 1, ul. "Gen. Vasil Kutinchev, 1343", Sofia, Bulgaria
             </a>
           </p>
         </div>
 
-        <div className="contact-container-right">
-          <p><strong>Phone: </strong> <a href="tel:+359898245509"><i className="fas fa-phone-alt"></i> +359 898 245 509</a></p>
-          <p><strong>LinkedIn: </strong> <a href="https://linkedin.com/in/marius-georgiev-444aa6285" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i> Marius-Georgiev</a></p>
+        <div className={styles["contact-container-right"]}>
+          <p><strong>Phone: </strong> <a href="tel:+359898245509"><i className={styles["fas fa-phone-alt"]}></i> +359 898 245 509</a></p>
+          <p><strong>LinkedIn: </strong> <a href="https://linkedin.com/in/marius-georgiev-444aa6285" target="_blank" rel="noopener noreferrer"><i className={styles["fab fa-linkedin"]}></i> Marius-Georgiev</a></p>
         </div>
       </div>
 
-      <div className="second-section">
-        <div className="contact-form-container">
+      <div className={styles["second-section"]}>
+        <div className={styles["contact-form-container"]}>
           <h2>Contact Us</h2>
           <form onSubmit={sendEmail}>
             <label htmlFor="name">Your Name</label>
@@ -116,7 +116,7 @@ const Contact = () => {
             <label htmlFor="message">Your Message</label>
             <textarea
               id="message"
-              rows="10"
+              rows="6"
               value={message}
               onChange={(e) => setMessage(e.target.value)}
               placeholder="Write your message here..."
@@ -130,7 +130,7 @@ const Contact = () => {
           {errorMessage && <p className="error">{errorMessage}</p>}
         </div>
 
-        <div className="google-map">
+        <div className={styles["google-map"]}>
           <h2>Find Me Here</h2>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d1465.4604439443951!2d23.25418687642453!3d42.726565169018386!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDLCsDQzJzM1LjYiTiAyM8KwMTUnMTcuOCJF!5e0!3m2!1sen!2sbg!4v1734236297702!5m2!1sen!2sbg"
